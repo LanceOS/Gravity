3d088a6d9649e4b733f8408499f568014296e880
LanceOS <136203815+LanceOS@users.noreply.github.com>
2026-07-06 14:34:05 -0400

    Remove legacy workspace chat page

--- client/src/modules/aiChat/index.ts
+++ client/src/modules/aiChat/index.ts
@@ -1,3 +1,1 @@
 export { AiChatDock } from './components/AiChatDock';
-export { WorkspaceChatDock } from './components/WorkspaceChatDock';
-export { WorkspaceChatPage } from './screens/WorkspaceChatPage';
--- library/components/aichat/index.ts
+++ library/components/aichat/index.ts
@@ -1,6 +1,5 @@
 export * from './AIChatWindow';
 export * from './AIChatMessage';
-export * from './ChatSidebar';
 export * from './FormattedMarkdown';
 export * from './ChatInterface';
 export * from './types';
